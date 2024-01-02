4da9a0309448961d882072c10c31f991e44a6f7d
hannes <hannes.rother@online.de>
2024-01-03 00:11:07 +0100

    we startin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="center-container">
+    <img src="https://placehold.co/500x300" alt="Placeholder Image" />
+    <h1 class="mt-14 font-bold text-3xl">Dörte Brügmann</h1>
+    <p class="text-xl mt-4">
+        Heilpraktikerin für Psychotherapie, Physiotherapeutin
+    </p>
+</section>
+
+<style>
+    .center-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh; /* Set the height to 100% of the viewport height */
+        background-color: #eca624;
+    }
+</style>
