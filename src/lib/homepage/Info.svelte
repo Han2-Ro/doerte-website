4706050025b1036e42a9caace7fb44e9d961a31a
Han2-Ro <hannes.rother@online.de>
2024-01-16 22:07:28 +0100

    Construction Site

--- src/lib/homepage/Info.svelte
+++ src/lib/homepage/Info.svelte
@@ -2,17 +2,19 @@
     import portrait from "$lib/assets/portrait.jpg";
 </script>
 
-<section
-    class="lg:mt-20 mb-20 flex flex-col-reverse lg:flex-row justify-center"
->
-    <img src={portrait} alt="Placeholder" />
+<section class="md:py-20 flex flex-col-reverse md:flex-row justify-center">
+    <img
+        src={portrait}
+        alt="Portrait von Dörte Brügmann"
+        class="max-h-64 object-contain"
+    />
     <div class="p-10 flex flex-col justify-evenly items-center">
-        <h1 class="font-bold text-3xl">Dörte Brügmann</h1>
+        <h2 class="font-bold text-3xl">Dörte Brügmann</h2>
         <p class="my-3 text-center">
             Heilpraktikerin für Psychotherapie, Physiotherapeutin
         </p>
         <p>Drausnickstr. 8, 91052 Erlangen</p>
         <p>Tel.: 0151 - 59178819</p>
-        <p>Email: doerte.bruegmann@online.de</p>
+        <p>Email: kontakt@praxis-bruegmann.de</p>
     </div>
 </section>
---
+++ src/lib/homepage/UnderConstruction.svelte
@@ -0,0 +1,14 @@
+<script>
+    import ConstructionSign from "$lib/assets/construction-sign_600.webp";
+</script>
+
+<section class="">
+    <div
+        class="py-20 px-8 flex flex-col justify-evenly items-center bg-pink-900"
+    >
+        <img src={ConstructionSign} alt="Baustellenschild" class="h-60" />
+        <h1 class="mt-10 text-white font-bold text-7xl text-center">
+            Website im Aufbau
+        </h1>
+    </div>
+</section>
