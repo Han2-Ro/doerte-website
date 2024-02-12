b642b484c6ef06adb9268ace6bac21d7f7a9fe02
Han2-Ro <hannes.rother@online.de>
2024-02-12 12:08:48 +0100

    added Footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,5 @@
+<footer class=" bg-green-300 p-10 text-center">
+    <p>Praxis für Körperarbeit & Traumatherapie - Dörte Brügmann - Drausnickstr. 8 - 91052 Erlangen</p>
+    <p>Tel.: 01515 - 9178819           kontakt@praxis-bruegmann.de
+</p>
+</footer>
