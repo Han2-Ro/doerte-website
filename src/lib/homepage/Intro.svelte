0a2009df9fe06eb6462f4ad16df46dffb6330519
hannes <hannes.rother@online.de>
2024-02-22 01:49:13 +0100

    added text

--- src/lib/homepage/Intro.svelte
+++ src/lib/homepage/Intro.svelte
@@ -4,7 +4,54 @@
 
 <section class="flex px-20 gap-20">
     <img class="h-[40rem]" src={daisies} alt="Gänseblühmchen in einem weichen violettem Licht">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mattis varius nulla eget fermentum. Etiam eu metus sed ligula tempor mattis at nec nisl. Duis varius leo erat, ac ullamcorper urna fermentum interdum. Duis placerat leo non aliquam luctus. Quisque et volutpat odio, in porta dui. Ut et ex ut eros vestibulum tincidunt non congue tellus. Nam tempus convallis laoreet. Nulla sit amet iaculis risus. Pellentesque ullamcorper orci vel ipsum congue, sed pretium sapien volutpat. Mauris nec condimentum nisi, a tempor velit. Praesent pretium augue urna, sed dapibus leo pellentesque id. Cras id vestibulum tellus.
+    <div>
 
-        Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris convallis, purus vitae commodo sagittis, elit lorem sollicitudin libero, quis condimentum risus ligula et nunc. Maecenas et leo nec sapien ultrices lacinia nec eget orci. Sed molestie vulputate nibh vel bibendum. Suspendisse tempus augue tincidunt consequat pellentesque. Vivamus facilisis pellentesque purus, elementum scelerisque mi aliquet a. Vivamus ex odio, commodo id lacinia at, posuere non dui. Etiam rutrum massa in odio dignissim, laoreet hendrerit mauris imperdiet. Nunc ac ultricies lorem. Praesent porttitor commodo nulla id egestas. Etiam mollis interdum ex efficitur viverra. Maecenas lobortis posuere enim, in accumsan nulla semper a. Nullam lobortis suscipit diam volutpat mollis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean laoreet nec orci ut venenatis. </p>
+        <p>
+            Herzlich Willkommen …
+        </p>
+        <p>
+… manchmal geschehen Dinge in unserem Leben,  die uns völlig aus unserem inneren Gleichgewicht bringen und die sich auf alle Ebenen unseres Seins auswirken können – seelisch, körperlich, emotional, unser Denken und Handeln und unsere Beziehungsfähigkeit betreffend.
+Dies können einzelne Ereignisse sein wie z.B. Unfälle, Stürze, Übergriffe, Verlusterlebnisse oder Anpassungsschwierigkeiten an eine neue Lebenssituation, oder auch sich über einen längeren Zeitraum wiederholende Ereignisse wie z.B. Missbrauch, Gewalterlebnisse, Vernachlässigung, Grenzverletzungen, Beschämungen.
+Wir fühlen uns dann vielleicht emotional überfordert, hilflos, handlungsunfähig, niedergeschlagen und in unserer Lebensenergie blockiert oder abgeschnitten von der Verbindung zu uns selbst, zu anderen oder zur Welt.
+Gerne begleite ich sie  therapeutisch in einer eingestimmten, urteilsfreien und respektvollen Art und Weise,  wenn sie….
+<p>
+    …mehr Verständnis über die Entstehungsweise ihrer Symptome und ihres Leidens entwickeln möchten
+</p>
+<p>
+    …lernen möchten, wie sie sich regulieren und in eine bessere innere Balance kommen können
+</p>
+<p>
+    …Unterstützung in Krisen oder bei Trauerprozessen wünschen
+</p>
+<p>
+    …einen besseren Umgang mit ihren Ängsten erlernen möchten
+</p>
+<p>
+    …erforschen wollen, wie sie wieder einen besseren Zugang zu ihrer Körperwahrnehmung bekommen
+</p>
+<p>
+    …Sehnsucht nach tieferen, authentischeren Beziehungen haben
+</p>
+<p>
+    …erfahren möchten, wie man sich in stressigen Situationen stabilisieren und erden kann
+</p>
+<p>
+    …wieder mehr in ihre Energie kommen möchten
+</p>
+<p>
+    …ihre Bedürfnisse wieder besser spüren und ihre  Grenzen besser kennenlernen möchten 
+</p>
+<p>
+    …Gefühle der Erstarrung wieder mehr in den Fluss bringen wollen
+</p>
+<p>
+    …sie ihren Weg zu mehr Resilienz gehen möchten
+</p>
+<p>
+    ……
+</p>
+<p>
+Bei Fragen oder zur Vereinbarung eines Erstgesprächs nehmen sie gerne telefonisch, per e-mail oder per WhatsApp Kontakt zu mir auf.
+</p>
+</div>
 </section>
