6c643ecfb43a735d7cc76e169cf26254135ff455
hannes <hannes.rother@online.de>
2024-02-23 15:16:24 +0100

    some formatting

--- src/lib/homepage/Info.svelte
+++ src/lib/homepage/Info.svelte
@@ -1,13 +1,21 @@
 <script>
 </script>
 
-<section class="py-10 flex flex-col-reverse md:flex-row justify-center">
-    <div class="p-10 flex flex-col justify-evenly items-center">
-        <h2 class="font-bold text-3xl text-center w-full">Dörte Brügmann • Heilpraktikerin für Psychotherapie, Physiotherapeutin</h2>
-        <p>Somatic Experiencing  Practitioner</p>
-        <p>Körperarbeit • Traumatherapie • Stressregulation</p>
-        <p>Physiotherapeutin</p>
-        <p>Krankengymnastik • Massagen</p>
-        <p class="pt-5">Drausnickstr. 8, 91052 Erlangen • Tel. : +49 1515 9178819 • Email: kontakt@praxis-bruegmann.de</p>
+<section class="mt-20 py-10 flex flex-col-reverse md:flex-row justify-center">
+    <div class="p-10 flex flex-col justify-evenly items-center text-lg">
+        <h2 class="font-bold text-3xl text-center w-full">Dörte Brügmann</h2>
+        <h2 class=" mt-3  text-3xl text-center w-full">Heilpraktikerin für Psychotherapie • Erlangen</h2>
+        <h3 class="mt-5">Somatic Experiencing  Practitioner</h3>
+        <p class="italic">Körperarbeit • Traumatherapie • Stressregulation</p>
+        <h3 class="mt-5">Physiotherapeutin</h3>
+        <p class="italic">Krankengymnastik • Massagen</p>
     </div>
 </section>
+
+<style>
+    h3 {
+        font-size: 1.5rem;
+        font-weight: 600;
+        font-family: 'Lucida Caligraphy', 'Lucida Handwriting', sans-serif;
+    }
+</style>
--- src/lib/homepage/Intro.svelte
+++ src/lib/homepage/Intro.svelte
@@ -2,18 +2,21 @@
     import daisies from "$lib/assets/daisies.webp";
 </script>
 
-<section class="flex px-20 gap-20">
+<section class=" mt-10 mb-10 flex px-20 gap-20 font-serif text-xl leading-8">
     <img class="h-[40rem]" src={daisies} alt="Gänseblühmchen in einem weichen violettem Licht">
     <div>
 
-        <p>
+        <h2 class=" py-4 text-2xl italic">
             Herzlich Willkommen …
-        </p>
+        </h2>
         <p>
 … manchmal geschehen Dinge in unserem Leben,  die uns völlig aus unserem inneren Gleichgewicht bringen und die sich auf alle Ebenen unseres Seins auswirken können – seelisch, körperlich, emotional, unser Denken und Handeln und unsere Beziehungsfähigkeit betreffend.
 Dies können einzelne Ereignisse sein wie z.B. Unfälle, Stürze, Übergriffe, Verlusterlebnisse oder Anpassungsschwierigkeiten an eine neue Lebenssituation, oder auch sich über einen längeren Zeitraum wiederholende Ereignisse wie z.B. Missbrauch, Gewalterlebnisse, Vernachlässigung, Grenzverletzungen, Beschämungen.
 Wir fühlen uns dann vielleicht emotional überfordert, hilflos, handlungsunfähig, niedergeschlagen und in unserer Lebensenergie blockiert oder abgeschnitten von der Verbindung zu uns selbst, zu anderen oder zur Welt.
-Gerne begleite ich sie  therapeutisch in einer eingestimmten, urteilsfreien und respektvollen Art und Weise,  wenn sie….
+<p class="mt-5">
+    Gerne begleite ich sie  therapeutisch in einer eingestimmten, urteilsfreien und respektvollen Art und Weise, <br> wenn sie….
+</p>
+<ul class="mt-5 leading-10">
 <p>
     …mehr Verständnis über die Entstehungsweise ihrer Symptome und ihres Leidens entwickeln möchten
 </p>
@@ -22,6 +25,9 @@ Gerne begleite ich sie  therapeutisch in einer eingestimmten, urteilsfreien und 
 </p>
 <p>
     …Unterstützung in Krisen oder bei Trauerprozessen wünschen
+</p>
+<p>
+    ...lernen möchten, ihre Ängste besser zu regulieren
 </p>
 <p>
     …einen besseren Umgang mit ihren Ängsten erlernen möchten
@@ -48,8 +54,12 @@ Gerne begleite ich sie  therapeutisch in einer eingestimmten, urteilsfreien und 
     …sie ihren Weg zu mehr Resilienz gehen möchten
 </p>
 <p>
+    ...als Angehörige(r) Unterstützung suchen
+</p>
+<p>
     ……
 </p>
+</ul>
 <p>
 Bei Fragen oder zur Vereinbarung eines Erstgesprächs nehmen sie gerne telefonisch, per e-mail oder per WhatsApp Kontakt zu mir auf.
 </p>
