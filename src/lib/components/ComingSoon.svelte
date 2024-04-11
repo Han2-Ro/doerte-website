6b1207469e587b2e4bc6c8028091348f9a50319c
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-04-11 21:59:13 +0200

    add title atribute

--- src/lib/components/ComingSoon.svelte
+++ src/lib/components/ComingSoon.svelte
@@ -1,6 +1,16 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        document.title = 'Coming Soon';
+    });
+
+    export let title: string;
+</script>
+
 <section>
     <div class="container mx-auto px-4 py-12">
-        <h2 class="text-3xl font-bold text-center">Coming Soon</h2>
-        <p class="mt-3 text-center">Diese Seite befindet sich noch im Aufbau. <br /> Schauen Sie bald wieder vorbei.</p>
+        <h2 class="text-4xl font-bold text-center">{title}</h2>
+        <p class="mt-3 text-xl text-center">Coming soon...</p>
     </div>
 </section>
