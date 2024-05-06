05f55f26eb0e66b446db503d1f9a56ef9cb22d4f
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-05-06 12:23:30 +0200

    add datenschutz-notizen

---
+++ resources/datenschutz-notizen.md
@@ -0,0 +1,31 @@
+# Daten
+
+## Hetzner Hosting
+https://docs.hetzner.com/de/general/general-terms-and-conditions/data-privacy-faq/
+Im Logfile:
+- IP-Adresse (anonymisiert)
+- verwendeter Browser
+- Uhrzeit und Datum
+- Betriebssystem
+
+kein Personenbezug möglich
+Löschung nach 14 Tagen.
+
+## Was wir *nicht* machen:
+Cookies
+Google Einbindungen (Fonts, Analytics, Maps, ...)
+Marketing Mails
+
+## Geplant: Kontaktformular
+- Email
+- Name
+- Telefonnummer
+- Nachricht
+- Zustimmung via Checkbox zur Speicherung der Daten
+
+Wie/Wo gespeichert: unklar, 
+    Möglichkeiten: auf Hetzner Server(--> AV-Vertrag)
+    Email: wahrscheinlich unverschlüsselt
+
+Dauer der Speicherung: Solange sie gebraucht werden.
+
--- src/lib/homepage/Info.svelte
+++ src/lib/homepage/Info.svelte
@@ -4,7 +4,7 @@
 <section class=" pt-xl px-xs flex flex-col-reverse md:flex-row justify-center">
     <div class="flex flex-col justify-evenly items-center text-center">
         <h2 class=" font-bold text-2xl lg:text-3xl text-center w-full font-handwriting text-logo-purple">Dörte Brügmann</h2>
-        <p class="text-md lg:text-xl text-center w-full">Heilpraktikerin für Psychotherapie • Erlangen</p>
+        <p class="text-md text-2xl text-center font-handwriting w-full">Heilpraktikerin für Psychotherapie • Erlangen</p>
         <h3 class="mt-md text-lg lg:text-2xl font-handwriting">Somatic Experiencing  Practitioner</h3>
         <p class="italic">Körperarbeit • Traumatherapie • Stressregulation</p>
         <h3 class="mt-md text-lg lg:text-2xl font-handwriting">Physiotherapeutin</h3>
