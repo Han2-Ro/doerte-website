811b8b5115ea00ecf2dc968ad94e60ad1b8aa5d8
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-05-06 15:25:52 +0200

    add datenschutz, impressum pages

---
+++ src/routes/impressum/+page.svelte
@@ -0,0 +1,38 @@
+<article class="prose py-20 px-5 mx-auto max-w-[84rem]">
+    <h1>Impressum</h1>
+    <p>
+        Max Mustermann<br />
+        Musterladen<br />
+        Musterstra&szlig;e 111<br />
+        Geb&auml;ude 44<br />
+        90210 Musterstadt
+    </p>
+    <h2>Kontakt</h2>
+    <p>
+        Telefon: +49 (0) 123 44 55 66<br />
+        E-Mail: mustermann@musterfirma.de
+    </p>
+    <h2>Umsatzsteuer-ID</h2>
+    <p>
+        Umsatzsteuer-Identifikationsnummer gem&auml;&szlig; &sect; 27 a
+        Umsatzsteuergesetz:<br />
+        DE999999999
+    </p>
+    <h2>Berufsbezeichnung und berufsrechtliche Regelungen</h2>
+    <p>
+        Berufsbezeichnung:<br />
+        Heilpraktikerin
+    </p>
+    <h2>
+        Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle
+    </h2>
+    <p>
+        Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren
+        vor einer Verbraucherschlichtungsstelle teilzunehmen.
+    </p>
+    <p>
+        Quelle: <a href="https://www.e-recht24.de/impressum-generator.html"
+            >https://www.e-recht24.de/impressum- generator.html</a
+        >
+    </p>
+</article>
