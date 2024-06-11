0aa56990eb9859501ccd5eb124768e0ff854973f
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-06-11 11:54:34 +0200

    clean

--- src/lib/homepage/Hero.svelte
+++ src/lib/homepage/Hero.svelte
@@ -4,7 +4,7 @@
 
 <section class="w-full h-[25rem] mb-20">
     <div
-        class=" max-w-[80rem] mx-auto flex items-center flex-col lg:flex-row-reverse px-sm pt-lg lg:p-lg relative"
+        class=" max-w-[80rem] mx-auto flex items-center flex-col lg:flex-row-reverse p-lg relative"
     >
         <img
             src={Poppies}
@@ -12,7 +12,7 @@
             class=" h-[15rem] w-[20rem] lg:h-[20rem] lg:w-[25rem] object-cover rounded-[50%] border-2 border-dark-green"
         />
         <div
-            class="p-sm shadow-md shadow-black/50 lg:max-w-[50rem] rounded-3xl bg-lime-900 absolute -bottom-20 inset-x-sm lg:left-lg font-handwriting text-yellow-400"
+            class="p-sm shadow-md shadow-black/50 lg:max-w-[50rem] rounded-3xl bg-lime-900 absolute bottom-0 inset-x-sm lg:left-lg font-handwriting text-yellow-400"
         >
             <p class="text-lg lg:text-2xl text-center">
                 „Und der Tag kam, an dem das Risiko, fest in der geschlossenen
