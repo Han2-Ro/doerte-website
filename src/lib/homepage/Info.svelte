25db43cdfa9ec9c01ed0cbf2af2ac4d7d818428a
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-06-11 10:39:07 +0200

    changes

--- src/lib/homepage/Info.svelte
+++ src/lib/homepage/Info.svelte
@@ -5,9 +5,9 @@
     <div class="flex flex-col justify-evenly items-center text-center">
         <h2 class=" font-bold text-2xl lg:text-3xl text-center w-full font-handwriting text-logo-purple">Dörte Brügmann</h2>
         <p class="text-md text-2xl text-center font-handwriting w-full text-logo-purple">Heilpraktikerin für Psychotherapie • Erlangen</p>
-        <h3 class="mt-md text-lg lg:text-2xl font-handwriting">Somatic Experiencing  Practitioner</h3>
+        <h3 class="mt-md text-lg lg:text-2xl font-handwriting text-logo-purple">Somatic Experiencing  Practitioner</h3>
         <p class="italic">Körperarbeit • Traumatherapie • Stressregulation</p>
-        <h3 class="mt-md text-lg lg:text-2xl font-handwriting">Physiotherapeutin</h3>
+        <h3 class="mt-md text-lg lg:text-2xl font-handwriting text-logo-purple">Physiotherapeutin</h3>
         <p class="italic">Krankengymnastik • Massagen</p>
     </div>
 </section>
