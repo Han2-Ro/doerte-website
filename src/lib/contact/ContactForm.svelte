3d334971aa2345a541f748046aeb07d95acd1354
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-07-18 13:43:16 +0200

    form layout

--- src/lib/contact/ContactForm.svelte
+++ src/lib/contact/ContactForm.svelte
@@ -1,12 +1,12 @@
-<form class="" action="https://api.staticforms.xyz/submit" method="post">
+<form class=" max-w-[80rem] mx-auto px-md py-xl flex flex-col" action="https://api.staticforms.xyz/submit" method="post">
     <input class="input-field" type="hidden" name="accessKey" value="581f061f-3033-4a65-a18a-98a3cb32d908">
     <label for="name">Name</label>
     <input class="input-field" type="text" name="name">
-    <label for="email">E-Mail</label>
+    <label class="pt-md" for="email">E-Mail</label>
     <input class="input-field" type="text" name="email">
-    <label for="message">Nachricht</label>
+    <label class="pt-md" for="message">Nachricht</label>
     <textarea class="input-field" name="message"></textarea>
-    <input class=" bg-slate-400 p-1 rounded border-2 border-slate-600" type="submit" value="Submit" />
+    <input class="mt-md w-28 p-xs bg-[#afc8fd] rounded-full border-2 border-slate-600" type="submit" value="Submit" />
 </form>
 
 <style>
