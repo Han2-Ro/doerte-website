94d96eb101ff840933ee5bd29289726b30dd478e
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-07-18 13:08:27 +0200

    improve hero

--- src/lib/contact/Hero.svelte
+++ src/lib/contact/Hero.svelte
@@ -4,15 +4,15 @@
 
 <section class="w-full h-[25rem] mb-20">
     <div
-        class=" max-w-[80rem] h-full mx-auto flex items-center flex-col lg:flex-row-reverse p-lg relative"
+        class=" max-w-[80rem] h-full mx-auto flex items-center flex-col lg:flex-row p-lg relative"
     >
         <img
             src={Poppies}
             alt="orangefarbene Mohnblumen"
-            class=" h-[15rem] w-[20rem] lg:h-[20rem] lg:w-[25rem] object-cover rounded-[50%] border-4 border-[#c1d1ec]"
+            class=" h-[15rem] w-[20rem] lg:h-[20rem] lg:w-[25rem] object-cover"
         />
         <div
-            class="p-sm shadow-md shadow-black/50 lg:max-w-[50rem] rounded-3xl bg-lime-900 absolute -bottom-20 inset-x-sm lg:left-lg font-handwriting text-yellow-400"
+            class="p-sm shadow-md shadow-black/50 lg:max-w-[50rem] rounded-3xl bg-[#afc8fd] absolute -bottom-20 lg:right-lg font-handwriting"
         >
             <p class="text-lg lg:text-2xl text-center">
                 “Safety is much more than the absence of threat, safety is the
