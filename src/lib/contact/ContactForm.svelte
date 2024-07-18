42220cd5f92c716d005525a82c585211034ba0fa
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-07-18 12:16:54 +0200

    form

---
+++ src/lib/contact/ContactForm.svelte
@@ -0,0 +1,19 @@
+<form class="" action="https://api.staticforms.xyz/submit" method="post">
+    <input class="input-field" type="hidden" name="accessKey" value="581f061f-3033-4a65-a18a-98a3cb32d908">
+    <label for="name">Name</label>
+    <input class="input-field" type="text" name="name">
+    <label for="email">E-Mail</label>
+    <input class="input-field" type="text" name="email">
+    <label for="message">Nachricht</label>
+    <textarea class="input-field" name="message"></textarea>
+    <input class=" bg-slate-400 p-1 rounded border-2 border-slate-600" type="submit" value="Submit" />
+</form>
+
+<style>
+    .input-field {
+        padding: 10px;
+        margin: 10px 0;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+    }
+</style>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,9 +1,10 @@
 <script>
     import ComingSoon from "$lib/components/ComingSoon.svelte";
+    import ContactForm from "$lib/contact/ContactForm.svelte";
 </script>
 
 <svelte:head>
     <title>Kontakt</title>
     <meta name="description" content="Kontaktinformationen von Dörte Brügmann">
 </svelte:head>
-<ComingSoon title="Kontakt"/>
+<ContactForm />
