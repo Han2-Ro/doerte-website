c659f5faaa991837ec290b8db819259942b74562
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-07-18 12:55:20 +0200

    add Hero

---
+++ src/lib/contact/Hero.svelte
@@ -0,0 +1,32 @@
+<script>
+    import Poppies from "$lib/assets/swan.png";
+</script>
+
+<section class="w-full h-[25rem] mb-20">
+    <div
+        class=" max-w-[80rem] h-full mx-auto flex items-center flex-col lg:flex-row-reverse p-lg relative"
+    >
+        <img
+            src={Poppies}
+            alt="orangefarbene Mohnblumen"
+            class=" h-[15rem] w-[20rem] lg:h-[20rem] lg:w-[25rem] object-cover rounded-[50%] border-4 border-[#c1d1ec]"
+        />
+        <div
+            class="p-sm shadow-md shadow-black/50 lg:max-w-[50rem] rounded-3xl bg-lime-900 absolute -bottom-20 inset-x-sm lg:left-lg font-handwriting text-yellow-400"
+        >
+            <p class="text-lg lg:text-2xl text-center">
+                “Safety is much more than the absence of threat, safety is the
+                presence of connection.”
+            </p>
+            <p class=" text-base lg:text-xl italic text-center">- Stephen Porges</p>
+        </div>
+    </div>
+</section>
+
+<style>
+    section {
+        background-image: url($lib/assets/water.webp);
+        background-size: cover;
+        background-position: center;
+    }
+</style>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,10 +1,12 @@
 <script>
     import ComingSoon from "$lib/components/ComingSoon.svelte";
     import ContactForm from "$lib/contact/ContactForm.svelte";
+    import Hero from "$lib/contact/Hero.svelte";
 </script>
 
 <svelte:head>
     <title>Kontakt</title>
     <meta name="description" content="Kontaktinformationen von Dörte Brügmann">
 </svelte:head>
+<Hero />
 <ContactForm />
