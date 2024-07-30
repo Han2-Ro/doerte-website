a1262a6e5ecfe7d278caaf604d1a750087defe9f
hannes <hannes.rother@online.de>
2024-07-30 12:03:52 +0200

    add content

---
+++ src/lib/kosten/Physio.svelte
@@ -0,0 +1,37 @@
+<script>
+    import Portrait from "$lib/assets/portrait.webp";
+</script>
+
+<section class=" max-w-[80rem] mx-auto flex flex-col lg:flex-row-reverse py-xl">
+    <div id="quote" class="lg:w-2/5 lg:flex flex-col-reverse hidden p-10 py-10">
+        <div
+            class=" p-5 rounded-3xl bg-[#292d38a2] text-white font-handwriting text-center"
+        >
+            <p class=" text-xl leading-relaxed">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                nec nisl eu nisi faucibus convallis vel id arcu. Proin non justo
+                non felis varius consequat. Interdum et malesuada fames ac ante
+                ipsum primis in faucibus.
+            </p>
+        </div>
+    </div>
+    <div class="px-sm lg:w-3/5">
+        Honorar Physiotherapie Nur private Kassen, Beihilfe, Selbstzahler Eine
+        Abrechnung über die gesetzliche Krankenversicherung ist leider nicht
+        möglich. Private Kassen Beihilfe und Selbstzahler Krankengymnastik:
+        35,00 31,00 KG-Atemtherapie: 35,00 31,00 KG-ZNS/Neuro (KG Bobath Erw.):
+        53,00 49,00 Massage: 29,00 25,00 Ausfallsregelung Ich bitte darum,
+        Termine mindestens 24 Stunden im Voraus zu verschieben oder abzusagen. 
+        ​ Termine, die weniger als 24 Stunden im Voraus verschoben oder abgesagt
+        werden, stelle ich mit 50% der Kosten in Rechnung.  Termine, die nicht
+        abgesagt wurden, muss ich zu 100% in Rechnung stellen.
+    </div>
+</section>
+
+<style>
+    #quote {
+        background-image: url($lib/assets/blowball.webp);
+        background-size: cover;
+        background-position: right top;
+    }
+</style>
--- src/routes/kosten/+page.svelte
+++ src/routes/kosten/+page.svelte
@@ -1,5 +1,7 @@
 <script>
     import Hero from "$lib/kosten/Hero.svelte";
+    import Physio from "$lib/kosten/Physio.svelte";
+    import Psycho from "$lib/kosten/Psycho.svelte";
 </script>
 
 <svelte:head>
@@ -7,4 +9,5 @@
     <meta name="description" content="Informationen zu den Kosten">
 </svelte:head>
 <Hero />
-<div class="py-lg"></div>
+<Psycho />
+<Physio />
