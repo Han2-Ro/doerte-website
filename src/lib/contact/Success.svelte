c95cb56713dfa0e31f38ff6b4c16ea010501b69a
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-10-19 22:04:42 +0200

    fix mobile success page

--- src/lib/contact/Success.svelte
+++ src/lib/contact/Success.svelte
@@ -1,5 +1,5 @@
 <section class=" flex flex-col items-center p-20">
-    <p class="text-2xl font-bold">Danke für die Nachricht!</p>
+    <p class="text-2xl text-center font-bold">Danke für die Nachricht!</p>
     <a
         href="/contact"
         class="mt-md w-28 p-xs bg-[#afc8fd] rounded-full border-2 border-slate-600 text-center"
