a326a57090f76d63acdd9cb3af380a8d20377035
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-11-05 10:56:10 +0100

    hero therapy

--- src/routes/therapy/+page.svelte
+++ src/routes/therapy/+page.svelte
@@ -1,9 +1,10 @@
 <script>
     import ComingSoon from "$lib/components/ComingSoon.svelte";
+    import Hero from "$lib/therapy/Hero.svelte";
 
 </script>
 
 <svelte:head>
     <title>Therapie</title>
 </svelte:head>
-<ComingSoon title="Therapie"/>
+<Hero/>
